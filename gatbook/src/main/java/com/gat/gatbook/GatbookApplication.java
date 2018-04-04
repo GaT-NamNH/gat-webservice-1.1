a5044a9a20497dfe14bbd23886969a08a06a28d6
namnh <namnh@evolableasia.vn>
2018-04-04 08:20:49 +0700

    gat init

---
+++ gatbook/src/main/java/com/gat/gatbook/GatbookApplication.java
@@ -0,0 +1,12 @@
+package com.gat.gatbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatbookApplication.class, args);
+	}
+}
